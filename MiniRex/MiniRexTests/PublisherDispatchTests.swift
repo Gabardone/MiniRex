304838fe7aad6be608402f8c9a08e2ea6d24c2e4
Óscar Morales Vivó <oscarmv@mac.com>
2019-04-07 13:47:23 -0700

    Added file for publisher dispatch tests.

---
+++ MiniRex/MiniRexTests/PublisherDispatchTests.swift
@@ -0,0 +1,14 @@
+//
+//  PublisherDispatchTests.swift
+//  MiniRexTests
+//
+//  Created by Óscar Morales Vivó on 3/28/19.
+//  Copyright © 2019 Óscar Morales Vivó. All rights reserved.
+//
+
+import XCTest
+import MiniRex
+
+class PublisherDispatchTests: XCTestCase {
+
+}
