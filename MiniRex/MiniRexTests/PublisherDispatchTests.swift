b0de385396c454b0b2027a56615b61fe5fe2367a
Óscar Morales Vivó <oscarmv@mac.com>
2019-04-17 22:22:40 -0700

    Added unit test for update dispatching publisher.

--- MiniRex/MiniRexTests/PublisherDispatchTests.swift
+++ MiniRex/MiniRexTests/PublisherDispatchTests.swift
@@ -15,9 +15,10 @@ class PublisherDispatchTests: XCTestCase {
 
         //  We use the getSpecific/setSpecific Dispatch API to associate data to our test queue and verify that we're
         //  executing in it (the assert methods blow up which is not what we want for tests).
-        let subscribeQueue = DispatchQueue(label: "SubscribeTestQueue")
-        let key = DispatchSpecificKey<Int>()
-        subscribeQueue.setSpecific(key: key, value: 7)
+        let subscribeQueueLabel = "UpdateTestQueue"
+        let subscribeQueue = DispatchQueue(label: subscribeQueueLabel)
+        let key = DispatchSpecificKey<String>()
+        subscribeQueue.setSpecific(key: key, value: subscribeQueueLabel)
 
         //  Expectation run when we subscribe like a semaphore.
         let subscribeExpectation = expectation(description: "Subscription")
@@ -26,8 +27,8 @@ class PublisherDispatchTests: XCTestCase {
         let publisher = Publisher<Int>(withSubscribeBlock: { (updateBlock) -> Subscription in
             let specificValue = DispatchQueue.getSpecific(key: key)
             XCTAssertNotNil(specificValue)
-            if let specificInt = specificValue {
-                XCTAssertEqual(specificInt, 7)
+            if let specificString = specificValue {
+                XCTAssertEqual(specificString, subscribeQueueLabel)
             }
             subscribeExpectation.fulfill()
             return Subscription(withUnsubscriber: {})
@@ -38,6 +39,39 @@ class PublisherDispatchTests: XCTestCase {
 
         //  Finally subscribe so we run all of the above.
         let subscription = dispatchedPublisher.subscribe { (integer) in
+        }
+
+        //  The timeout means the test will technically fail if we stop in the debugger while waiting.
+        waitForExpectations(timeout: 1.0, handler: nil)
+
+        subscription.invalidate()
+    }
+
+
+    func testUpdateDispatch() {
+
+        //  We use the getSpecific/setSpecific Dispatch API to associate data to our test queue and verify that we're
+        //  executing in it (the assert methods blow up which is not what we want for tests).
+        let updateQueueLabel = "UpdateTestQueue"
+        let updateQueue = DispatchQueue(label: updateQueueLabel)
+        let key = DispatchSpecificKey<String>()
+        updateQueue.setSpecific(key: key, value: updateQueueLabel)
+
+        //  Expectation run when we subscribe like a semaphore.
+        let updateExpectation = expectation(description: "Update")
+
+        //  Build up the publisher that dispatches to our queue.
+        let publishedProperty = PublishedProperty<Int>(withInitialValue: 7)
+        let dispatchedProperty = publishedProperty.publishedValue.dispatch(updateQueue: updateQueue)
+
+        //  Update from subscription tests all the things.
+        let subscription = dispatchedProperty.subscribe { (integer) in
+            let specificValue = DispatchQueue.getSpecific(key: key)
+            XCTAssertNotNil(specificValue)
+            if let specificString = specificValue {
+                XCTAssertEqual(specificString, updateQueueLabel)
+            }
+            updateExpectation.fulfill()
         }
 
         //  The timeout means the test will technically fail if we stop in the debugger while waiting.
