04fdbb048cd869386fff02ce90191418fa050c29
Óscar Morales Vivó <oscarmv@mac.com>
2019-07-08 23:16:51 -0700

    Added non-progress task transformations. Updated documentation for task transformations.

--- MiniRex/MiniRex/Tasks/Task+Transform.swift
+++ MiniRex/MiniRex/Tasks/Task+Transform.swift
@@ -18,7 +18,7 @@ extension Task {
      update semantics, used the respective value transform API.
      - Parameter sourcePublisher: The publisher whose updates are the source for the one we're building.
      - Parameter transformationBlock: A block that converts a source update into one of the kind we want for the
-     publisher we're creating.
+     task we're creating.
 
      The transformationBlock can transform into any type whatsoever, including the source's type, so there is no
      limitation about it other than it has to produce a value for every source update.
@@ -30,26 +30,67 @@ extension Task {
             })
         })
     }
-}
 
-
-extension Task {
 
     /**
      Builds up a Task that transforms the updates from the caller into new values.
 
-     Note that this can be called on any Publisher but the return is a Broadcaster. This is because we can't make
-     any guarantees on the behavior of the result and  Broadcaster models that best. For Published<Value> there's
-     a separate API that returns another one.
-     - Parameter transformationBlock: A block that converts the caller's updates into ones of the kind we want for
-     the publisher we're creating.
-     - Returns: A broadcaster whose subscribers will get the same updates as the caller, but transformed by the
-     given block.
+     Note that this can be called on any Task whether it posts progress updates or not, although you'll need to
+     add some redundant logic to deal with a progress update that would never be called.
+     - Parameter transformationBlock: A block that converts the caller's task updates into one of the type requested
+     for the resulting Task.
+     - Returns: A Task whose subscribers will get the same updates as the caller, but transformed by the given block.
 
      The transformationBlock can transform into any type whatsoever, including the source's type, so there is no
-     limitation about it other than it has to produce a value for every source update.
+     limitation about it other than it has to be able to produce an update for every given task update.
      */
     public func transform<TransformedProgress, TransformedSuccess, TransformedFailure>(with transformationBlock: @escaping (Update) -> Task<TransformedProgress, TransformedSuccess, TransformedFailure>.Update) -> Task<TransformedProgress, TransformedSuccess, TransformedFailure> {
         return Task<TransformedProgress, TransformedSuccess, TransformedFailure>(withSource: self, transformationBlock: transformationBlock)
+    }
+}
+
+
+extension Task where Progress == Never {
+
+    /**
+     Builds a task that transforms another task's updates for cases where we don't care about progress reporting.
+
+     May be used as long as the resulting task has Never as its progress type. Only needs to transform the result.
+     - Parameter sourcePublisher: The publisher whose updates are the source for the one we're building.
+     - Parameter resultTransformationBlock: A block that converts a source update's result into one of the kind we
+     want for the task we're creating.
+
+     The transformationBlock can transform into any type whatsoever, including the source's type, so there is no
+     limitation about it other than it has to produce a value for any given task result. Progress updates will be
+     skipped.
+     */
+    init<OriginalProgress, OriginalSuccess, OriginalFailure>(withSource sourcePublisher: Task<OriginalProgress, OriginalSuccess, OriginalFailure>, resultTransformationBlock: @escaping (Result<OriginalSuccess, OriginalFailure>) -> Result<Success, Failure>) {
+        self.init(withSubscribeBlock: { (updateBlock: @escaping UpdateBlock) in
+            return sourcePublisher.subscribe({ (update) in
+                switch update {
+                case .inProgress(_):
+                    break
+
+                case .completed(let result):
+                    updateBlock(.completed(withResult: resultTransformationBlock(result)))
+                }
+            })
+        })
+    }
+
+
+    /**
+     Builds up a Task that transforms the result from the caller into a new result type, skipping progress updates.
+
+     Note that this can be called on any Task whether it posts progress updates or not.
+     - Parameter resultTransformationBlock: A block that converts the caller's task result into one of the type
+     requested for the resulting Task.
+     - Returns: A Task whose subscribers will get the same result as the caller, but transformed by the given block.
+
+     The resultTransformationBlock can transform into any type whatsoever, including the source's type, so there is
+     no limitation about it other than it has to be able to produce a value for every given task result.
+     */
+    public func transform<TransformedSuccess, TransformedFailure>(with resultTransformationBlock: @escaping (Result<Success, Failure>) -> Result<TransformedSuccess, TransformedFailure>) -> Task<Never, TransformedSuccess, TransformedFailure> {
+        return Task<Never, TransformedSuccess, TransformedFailure>(withSource: self, resultTransformationBlock: resultTransformationBlock)
     }
 }
