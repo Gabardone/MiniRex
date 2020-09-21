085a0dbf3d87ba07b24db7ac448b1073f7741fd9
Óscar Morales Vivó <oscarmv@mac.com>
2020-09-20 18:31:27 -0700

    Testing Swift Package Manager support.

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MiniRex",
+    platforms: [
+        .macOS(.v10_9),
+        .iOS(.v9_3),
+        .watchOS(.v2_2),
+        .tvOS(.v9_2)
+    ],
+    products: [
+        .library(
+            name: "MiniRex",
+            targets: ["MiniRex"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MiniRex",
+            dependencies: [],
+            path: "./MiniRex"
+        ),
+        .testTarget(
+            name: "MiniRexTests",
+            dependencies: ["MiniRex"],
+            path: "./MiniRexTests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
