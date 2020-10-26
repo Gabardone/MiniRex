24acfea3ef1bf7c41e566aa3f300dc2a5f6b19c0
Óscar Morales Vivó <oscarmv@mac.com>
2020-10-25 23:07:34 -0700

    Better delayed subscription API

--- MiniRex/MiniRex/Publisher+Delayed.swift
+++ MiniRex/MiniRex/Publisher+Delayed.swift
@@ -9,7 +9,14 @@
 import Foundation
 
 
+extension DispatchQueue {
+
+    fileprivate static let miniRexDelay = DispatchQueue(label: "org.omv.MiniRex.delay")
+}
+
+
 extension Publisher {
+
 
     /**
      Creates a publisher that subscribes at a given point in time.
@@ -18,30 +25,39 @@ extension Publisher {
      initial updates when subscribing to published values and will prevent receiving any notifications from
      broadcasters.
 
-     If this publisher goes away before the subscription with the source happens it will cancel that subscription
      - Note: It's best not to rely solely on subscription canceling too much since this functionality involves timers
      and it may be subject to race conditions.
+     - Note: Delay happens in a private concurrent queue, subject to change between versions. Make sure to dispatch
+     the source publisher's subscription to a known queue if it is important to ensure it executes on a particular
+     one.
      - Parameter publisher: The source publisher. We'll be making its subscription happen later.
-     - Parameter when: When to subscribe to the source publisher.
+     - Parameter delay: How long to delay the subscription.
      */
-    public init(withSourcePublisher publisher: Self, after delay: DispatchTimeInterval, subscribeQueue queue: DispatchQueue, dispatchOptions options: DispatchWorkItemFlags = .barrier) {
+    public init(withSourcePublisher publisher: Self, after delay: DispatchTimeInterval) {
         self.init { (updateBlock) -> Subscription in
+            //  We'll be acquiring a lock when either subscribing or unsubscribing as executing both concurrently
+            //  can only end in undefined, occasional and impossible to debug tears.
+            var unfairLock = os_unfair_lock_s()
             //  This variable is in the scope of this particular subscription so this should work if there's more
             //  than one subscription to the publisher.
             var sourcePublisher: Self? = publisher
             return Subscription(subscriptionWrapper: { (subscriptionBlock) in
-                queue.asyncAfter(deadline: .now() + delay, execute: {
+                DispatchQueue.miniRexDelay.asyncAfter(deadline: .now() + delay, execute: {
+                    os_unfair_lock_lock(&unfairLock)
                     //  We check for sourcePublisher only here so to we ensure we check on the right queue.
                     if let sourcePublisher = sourcePublisher {
                         subscriptionBlock(sourcePublisher.subscribe(updateBlock))
                     }
+                    os_unfair_lock_unlock(&unfairLock)
                 })
             }, unsubscriptionWrapper: { (unsubscriptionBlock) in
-                queue.async(group: nil, qos: .unspecified, flags: options, execute: {
+                DispatchQueue.miniRexDelay.async {
+                    os_unfair_lock_lock(&unfairLock)
                     //  If we land here we want to make sure subscription doesn't happen.
                     sourcePublisher = nil
                     unsubscriptionBlock()
-                })
+                    os_unfair_lock_unlock(&unfairLock)
+                }
             })
         }
     }
@@ -51,15 +67,12 @@ extension Publisher {
      Produces a new Publisher based on the caller that dispathces subscribe/unsubscribe operations to the given queue.
 
      Subscription/Unsubscription operations are dispatched with a barrier as they are semantically write operations.
-     - Parameter queue: The queue where we want subscription and unusbscription operations to happen.
-     - Parameter options: Options to use when dispatching subscribe/unsubscribe operations. The default is to dispatch
-     them as a barrier as they are smeantically write operations (and will usually behave like that in the ultimate
-     publisher source). This flag has no impact on serial queues so it can be left as a default even if it's known
-     they are being dispatched to one.
+     - Parameter delay: The delay to wait before subscription happens. See the documentation of
+     `init(withSourcePublisher:after:)` for more detail on behavior.
      - Returns: A publisher with the same behavior as the caller but which dispatches subscribe/unsubscribe work to the
      given queue.
      */
-    public func dispatch(after delay: DispatchTimeInterval, subscribeQueue queue: DispatchQueue, dispatchOptions options: DispatchWorkItemFlags = .barrier) -> Self {
-        return Self(withSourcePublisher: self, after: delay, subscribeQueue: queue, dispatchOptions: options)
+    public func dispatch(after delay: DispatchTimeInterval) -> Self {
+        return Self(withSourcePublisher: self, after: delay)
     }
 }
--- MiniRex/MiniRexTests/PublisherDelayedTests.swift
+++ MiniRex/MiniRexTests/PublisherDelayedTests.swift
@@ -12,14 +12,6 @@ import MiniRex
 class PublisherDelayedTests: XCTestCase {
 
     func testSubscribeDelayedUnsubscribeBeforeUpdates() {
-
-        //  We use the getSpecific/setSpecific Dispatch API to associate data to our test queue and verify that we're
-        //  executing in it (the assert methods blow up which is not what we want for tests).
-        let subscribeQueueLabel = "UpdateTestQueue"
-        let subscribeQueue = DispatchQueue(label: subscribeQueueLabel)
-        let key = DispatchSpecificKey<String>()
-        subscribeQueue.setSpecific(key: key, value: subscribeQueueLabel)
-
         //  Basic publisher does nothing but fail since we're expecting that actual subscription won't happen.
         let publisher = MiniRex.Published<Int>(withSubscribeBlock: { (updateBlock) -> Subscription in
             XCTFail("We shouldn't hit inner subscription in this test.")
@@ -27,7 +19,7 @@ class PublisherDelayedTests: XCTestCase {
         })
 
         //  Build the adapter that will dispatch subscription logic into our test queue half a second later.
-        let dispatchedPublisher = publisher.dispatch(after: .milliseconds(500), subscribeQueue: subscribeQueue)
+        let dispatchedPublisher = publisher.dispatch(after: .milliseconds(500))
 
         //  Finally subscribe so we run all of the above.
         let subscription = dispatchedPublisher.subscribe { (integer) in
@@ -46,30 +38,17 @@ class PublisherDelayedTests: XCTestCase {
      time we wait for fulfilled expectations.
      */
     func testSubscribeDelayed() {
-
-        //  We use the getSpecific/setSpecific Dispatch API to associate data to our test queue and verify that we're
-        //  executing in it (the assert methods blow up which is not what we want for tests).
-        let subscribeQueueLabel = "UpdateTestQueue"
-        let subscribeQueue = DispatchQueue(label: subscribeQueueLabel)
-        let key = DispatchSpecificKey<String>()
-        subscribeQueue.setSpecific(key: key, value: subscribeQueueLabel)
-
         //  Expectation run when we subscribe like a semaphore.
         let subscribeExpectation = expectation(description: "Subscription")
 
         //  Basic publisher does nothing but run our testing logic and return an empty Subscription.
         let publisher = MiniRex.Published<Int>(withSubscribeBlock: { (updateBlock) -> Subscription in
-            let specificValue = DispatchQueue.getSpecific(key: key)
-            XCTAssertNotNil(specificValue)
-            if let specificString = specificValue {
-                XCTAssertEqual(specificString, subscribeQueueLabel)
-            }
             subscribeExpectation.fulfill()
             return Subscription(withUnsubscriber: {})
         })
 
         //  Build the adapter that will dispatch subscription logic into our test queue.
-        let dispatchedPublisher = publisher.dispatch(after: .milliseconds(100), subscribeQueue: subscribeQueue)
+        let dispatchedPublisher = publisher.dispatch(after: .milliseconds(100))
 
         //  Finally subscribe so we run all of the above.
         let subscription = dispatchedPublisher.subscribe { (integer) in
